9a0ad6051cd86acc501d35f890a80a266cd3ecdc
guanyu7778 <157423346@qq.com>
2025-08-02 18:26:21 +0800

    Update compile.sh

--- buildsystem/compile.sh
+++ buildsystem/compile.sh
@@ -327,6 +327,7 @@ if [ -z "$VLC_SRC_DIR" ]; then
     fi
 
     (cd ${VLC_LIBJNI_PATH} && ./buildsystem/get-vlc.sh ${get_vlc_args})
+	ls
 	cp vlc/share/org.videolan.vlc.appdata.xml.in.in vlc/share/org.videolan.vlc.appdata.xml.tmp
 	cp vlc/share/org.videolan.vlc.appdata.xml.in.in vlc/share/org.videolan.vlc.appdata.xml
 	
